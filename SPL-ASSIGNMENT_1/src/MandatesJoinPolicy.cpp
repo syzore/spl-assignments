8380f9fa0b12cf98a481c9ed8e2bc3ad7863e573
Ofri Hanochi <ofrihano@post.bgu.ac.il>
2022-11-19 16:04:22 +0200

    123

--- SPL-ASSIGNMENT_1/src/MandatesJoinPolicy.cpp
+++ SPL-ASSIGNMENT_1/src/MandatesJoinPolicy.cpp
@@ -3,9 +3,19 @@
 
 void MandatesJoinPolicy::join(Party &p, Simulation &s)
 {
+    Coalition* favorite = nullptr;  //creats a dummy coalition to start with
+    int mostMandates = -1;
     for (int partyId : offers)
     {
+        Coalition temp = s.getCoalitionByPartyId(partyId);
+        int coalitionMandates = temp.getTotalMandates();
+        if (coalitionMandates > mostMandates)
+        {
+            mostMandates = coalitionMandates;
+            favorite = &temp;
         }
+    }
+    favorite->addParty(p);
 }
 
 void MandatesJoinPolicy::addOffer(int partyId)
