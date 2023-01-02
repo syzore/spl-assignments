ea75f1d5dbebc4881a685a8457784cf217bfe7dc
aviv profesorsky <aviv1pro@gmail.com>
2023-01-02 12:01:28 +0200

    updated assignment 3 template files

--- SPL-ASSIGNMENT_3/server/src/main/java/bgu/spl/net/api/StompMessagingProtocol.java
+++ SPL-ASSIGNMENT_3/server/src/main/java/bgu/spl/net/api/StompMessagingProtocol.java
@@ -2,13 +2,13 @@ package bgu.spl.net.api;
 
 import bgu.spl.net.srv.Connections;
 
-public interface StompMessagingProtocol  {
+public interface StompMessagingProtocol<T>  {
 	/**
 	 * Used to initiate the current client protocol with it's personal connection ID and the connections implementation
 	**/
-    void start(int connectionId, Connections<String> connections);
+    void start(int connectionId, Connections<T> connections);
     
-    void process(String message);
+    void process(T message);
 	
 	/**
      * @return true if the connection should be terminated
