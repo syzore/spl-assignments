b04ab327f3da3aa5f0336c40f88e9b5362c6ef9c
Ofri Hanochi <ofrihano@post.bgu.ac.il>
2023-01-02 18:17:47 +0200

    connectionimpl

---
+++ SPL-ASSIGNMENT_3/server/src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
@@ -0,0 +1,24 @@
+package bgu.spl.net.srv;
+
+public class ConnectionsImpl <T> implements Connections <T> {
+
+
+    @Override
+    public boolean send(int connectionId, T msg) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void send(String channel, T msg) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void disconnect(int connectionId) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
