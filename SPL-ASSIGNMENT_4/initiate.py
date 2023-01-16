dc0465d0bcc11af24c3390250bbbbc1b14f24bb6
aviv <aviv1pro@gmail.com>
2023-01-16 12:57:17 +0200

    initialized tables

--- SPL-ASSIGNMENT_4/initiate.py
+++ SPL-ASSIGNMENT_4/initiate.py
@@ -1,30 +1,48 @@
 from persistence import *
+from typing import List
 
 import sys
 import os
 
-def add_branche(splittedline : list[str]):
-    #TODO: add the branch into the repo
+
+def add_branche(splittedline: List[str]):
+    id, location, number_of_employees = splittedline
+    repo.execute_command(
+        f"INSERT INTO branches values ({id}, '{location}', {number_of_employees})")
     pass
 
-def add_supplier(splittedline : list[str]):
-    #TODO: insert the supplier into the repo
+
+def add_supplier(splittedline: List[str]):
+    id, name, contact_information = splittedline
+    repo.execute_command(
+        f"INSERT INTO suppliers values ({id}, '{name}', '{contact_information}')")
+
     pass
 
-def add_product(splittedline : list[str]):
-    #TODO: insert product
+
+def add_product(splittedline: List[str]):
+    id, description, price, quantity = splittedline
+    repo.execute_command(
+        f"INSERT INTO products values ({id}, '{description}', {price}, {quantity})")
+
     pass
 
-def add_employee(splittedline : list[str]):
-    #TODO: insert employee
+
+def add_employee(splittedline: List[str]):
+    id, name, salary, branche_id = splittedline
+    repo.execute_command(
+        f"INSERT INTO employees values ({id}, '{name}', {salary}, {branche_id})")
+
     pass
 
-adders = {  "B": add_branche,
-            "S": add_supplier,
-            "P": add_product,
-            "E": add_employee}
 
-def main(args : list[str]):
+adders = {"B": add_branche,
+          "S": add_supplier,
+          "P": add_product,
+          "E": add_employee}
+
+
+def main(args: List[str]):
     inputfilename = args[1]
     # delete the database file if it exists
     repo._close()
@@ -35,8 +53,9 @@ def main(args : list[str]):
     repo.create_tables()
     with open(inputfilename) as inputfile:
         for line in inputfile:
-            splittedline : list[str] = line.strip().split(",")
+            splittedline: List[str] = line.strip().split(",")
             adders.get(splittedline[0])(splittedline[1:])
+
 
 if __name__ == '__main__':
     main(sys.argv)
